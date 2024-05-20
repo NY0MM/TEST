b4cf056539c565a905b56c88b311af3ca9873e33
NY0MM <nyomltd@gmail.com>
2024-05-20 10:55:11 +0100

    Update params_file.py

--- params_file.py
+++ params_file.py
@@ -1,10 +1,8 @@
 params = [
-    {
+     {
         "monthlySold_gte": 50,
         "deltaPercent1_BUY_BOX_SHIPPING_gte": 40,
         "deltaPercent1_AMAZON_gte": 40,
-        "deltaPercent90_AMAZON_gte": 40,
-        "offerCountFBA_gte": 3,
         "productGroup": [
             "automotive parts and accessories",
             "beauty",
@@ -26,17 +24,17 @@ params = [
             "toy",
             "wireless"
         ],
+        "deltaPercent90_BUY_BOX_SHIPPING_gte": 40,
+        "avg90_BUY_BOX_SHIPPING_gte": 900,
+        "avg90_COUNT_NEW_gte": 4,
+        "productType": [
+            "0"
+        ],
         "sort": [
             [
                 "current_SALES",
                 "asc"
             ]
-        ],
-        "lastOffersUpdate_gte": 7033470,
-        "productType": [
-            0,
-            1,
-            2
         ],
         "perPage": 100,
         "page": 0
