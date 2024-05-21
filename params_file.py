c96e6beeeac0820b30eb2ecefc5e8deb64652463
NY0MM <nyomltd@gmail.com>
2024-05-21 19:12:13 +0100

    Update params_file.py

--- params_file.py
+++ params_file.py
@@ -1,45 +1,59 @@
 params = [
      {
-        "monthlySold_gte": 50,
-        "deltaPercent1_BUY_BOX_SHIPPING_gte": 40,
-        "deltaPercent1_AMAZON_gte": 40,
-        "productGroup": [
-            "automotive parts and accessories",
-            "beauty",
-            "book",
-            "digital music purchase",
-            "drugstore",
-            "electronics",
-            "furniture",
-            "home",
-            "home improvement",
-            "jewelry",
-            "kitchen",
-            "lawn & patio",
-            "luggage",
-            "office product",
-            "personal computer",
-            "pet products",
-            "sports",
-            "toy",
-            "video games",
-            "wireless"
-        ],
-        "deltaPercent90_BUY_BOX_SHIPPING_gte": 40,
-        "avg90_BUY_BOX_SHIPPING_gte": 900,
-        "avg90_COUNT_NEW_gte": 4,
-        "productType": [
-            "0"
-        ],
-        "sort": [
-            [
-                "current_SALES",
-                "asc"
-            ]
-        ],
-        "perPage": 100,
-        "page": 0
-    }
+    "monthlySold_gte": 50,
+    "avg90_BUY_BOX_SHIPPING_gte": 1600,
+    "deltaPercent90_BUY_BOX_SHIPPING_gte": 60,
+    "deltaPercent1_AMAZON_gte": 60,
+    "deltaPercent90_AMAZON_gte": 60,
+    "avg90_COUNT_NEW_gte": 3,
+    "productGroup": [
+        "✜amazon fresh ambient",
+        "✜amazon fresh deli and prepared meals",
+        "✜amazon fresh produce",
+        "✜amazon prints",
+        "✜apparel",
+        "✜art",
+        "✜audible",
+        "✜consumables email gift cards",
+        "✜consumables physical gift cards",
+        "✜designer fashion",
+        "✜digital accessories",
+        "✜digital accessories 2",
+        "✜digital accessories 4",
+        "✜digital book service",
+        "✜digital devices 10 accessories",
+        "✜digital devices 12 accessories",
+        "✜digital devices 3 accessories",
+        "✜digital devices 4",
+        "✜digital devices 5 accessories",
+        "✜digital devices 9 accessories",
+        "✜digital ebook purchas",
+        "✜digital music purchase",
+        "✜digital periodicals",
+        "✜digital products 3",
+        "✜digital products 7",
+        "✜digital software",
+        "✜digital text",
+        "✜digital text 2",
+        "✜digital video games",
+        "✜ebook",
+        "✜gift card",
+        "✜shoes",
+        "✜target gift card",
+        "✜video rental by mail"
+    ],
+    "productType": [
+        "0"
+    ],
+    "sort": [
+        [
+            "current_SALES",
+            "asc"
+        ]
+    ],
+    "perPage": 100,
+    "page": 0
+}
      
 ]
 
